5a6831fe552c882ef1369e56c0f3a38a54691bb3
Raphael Qureshi <76881227+raphc43@users.noreply.github.com>
2021-11-16 00:26:51 +0500

    Update expenses_controller.py

--- expenses_controller.py
+++ expenses_controller.py
@@ -1,21 +1,21 @@
 import math
 
-def estimate(terms=0, total_limit=0): # ax1 + ax2..... axN <= total_limit.
+def estimate(terms=0, total_limit=0): # aq1 + aq2..... aqN <= total_limit.
 	try:
-		x = total_limit/terms # Value of x or x <= val.
+		q = total_limit/terms # Value of q or x <= val.
 
 	except ZeroDivisionError:
 		print("Cannot divide by zero")
 
 	else:
-		x_rounded = math.floor(x) # Rounds the value of 'x' to the greatest integer.
+		q_rounded = math.floor(q) # Rounds the value of 'q' to the greatest integer.
 
 		# Prints the value of x, first without being rounded and then rounded.
-		print(x, 'or', x_rounded, 'can satistfy the inequality', end='\n') 
+		print(q, 'or', q_rounded, 'can satistfy the inequality', end='\n') 
 
 		'''Substitutes the value of 'x' with the rounded integer and solves the 
 		inequality'''
-		print('After Substitution:', (terms * x_rounded), '<=', total_limit) 
+		print('After Substitution:', (terms * q_rounded), '<=', total_limit) 
 
 
 # An example
